e47869ba86a845d6f05928149ea984aa589de424
CharlieMcr <charlie@charliemcr.com>
2016-03-23 23:02:30 +0000

    upgrade to 5.2

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -23,22 +23,3 @@ class DatabaseSeeder extends Seeder {
 	}
 
 }
-
-
-class VenueTableSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        //
-        // Uncomment the below to wipe the table clean before populating
-        // DB::table('venue')->delete();
-        ApiCalls::seed(65,'etihad-stadium','Etihad Stadium');
-        ApiCalls::seed(66,'old-trafford','Old Trafford');
-        // ApiCalls::mufc();
-    }
-}
