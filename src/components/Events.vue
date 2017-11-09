f06809e44f12942af3de5753da98de5a7b340ad0
charlie <charlie@charliemcr.com>
2017-11-09 18:13:12 +0000

    Adds localStorage caching for events

--- src/components/Events.vue
+++ src/components/Events.vue
@@ -29,17 +29,43 @@
     methods: {
       fetchEvents () {
         axios.get('https://api.tramatic.co.uk/events').then(results => {
-          this.events = results.data
+          this.storeEvents(results.data)
+          this.updateDisplayedEvents()
         })
       },
       formatDate (date) {
         let eventDate = moment(date)
         return eventDate.utcOffset(eventDate.utcOffset()).format('LLLL')
+      },
+      storeEvents (events) {
+        localStorage.setItem('events', JSON.stringify(events))
+        localStorage.setItem('events-updated', moment().format())
+      },
+      getEventsFromStorage () {
+        return localStorage.getItem('events')
+      },
+      updateDisplayedEvents () {
+        this.events = JSON.parse(this.getEventsFromStorage(), (key, value) => {
+          return value
+        })
+      },
+      displayEvents () {
+        let lastUpdateTime = localStorage.getItem('events-updated')
+        let now = moment(lastUpdateTime)
+        this.updateDisplayedEvents()
+        if (lastUpdateTime && now.isValid()) {
+          if (moment().diff(now, 'minutes') > 30) {
+            this.fetchEvents()
+            return
+          }
+          return
+        }
+        this.fetchEvents()
       }
     },
     mounted: function () {
       this.$nextTick(function () {
-        this.fetchEvents()
+        this.displayEvents()
       })
     }
   }
