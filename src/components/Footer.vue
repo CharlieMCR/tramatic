8f958f46cec18b521c765ee19951aaf93e5ce070
charlie <charlie@charliemcr.com>
2017-11-23 19:25:36 +0000

    Adds footer with version

---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+    <footer>
+        <a href="https://github.com/CharlieMCR/tramatic/releases" target="_blank">Version {{ version }}</a>
+    </footer>
+</template>
+
+<script>
+  /* global VERSION:true */
+  /* eslint no-undef: "error" */
+  export default {
+    name: 'Footer',
+    data () {
+      return {
+        version: VERSION
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
